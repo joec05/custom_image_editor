a31784747a754d8e11b7270db770179eec8f64a7
joec05 <joeclarence0510@gmail.com>
2023-07-28 18:31:31 +0700

    remove unused imports

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 1.0.0
 
 Earliest version of custom_image_editor
+
+## 1.0.1
+
+Update license
--- lib/provider/EditImageProvider.dart
+++ lib/provider/EditImageProvider.dart
@@ -1,6 +1,4 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 enum EditType{
   none,
